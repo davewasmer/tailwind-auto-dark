274e8464c38a07d76f9057a14880f3a8b211b70e
Dave Wasmer <davewasmer@gmail.com>
2023-10-21 00:35:49 -0600

    fix: publish types

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,11 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
+import ts from "rollup-plugin-ts";
 import pkg from "./package.json" assert { type: "json" };
 
 export default {
   input: "src/main.ts",
-  plugins: [typescript(), nodeResolve(), commonjs()],
+  plugins: [ts(), nodeResolve(), commonjs()],
   output: [
     { file: pkg.main, format: "cjs" },
     { file: pkg.module, format: "esm" },
